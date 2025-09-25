244ad72eeba6fd70c9535215e7bdf72619d6af88
ebdeuslave <abderrahim.essaouaf@gmail.com>
2025-09-25 15:11:32 +0100

    backup shipments history

--- Tawssil.py
+++ Tawssil.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 
@@ -26,12 +27,17 @@ class Tawssil:
         return sess
                
                
-    def getPackages(self, total_pages=100):
+    def getPackages(self, limit=80):
+        if limit > 80:
+            raise Exception("Limit should not be > 80")
+        
+        OFFSET = 0 # must be fixed to 0 to begin from the 1st page
         packages = {
             "total": 0,
             "list": []
         }
-        for offset in range(80, total_pages):
+        
+        for _ in range(0, 1000):
             json_data = {
                 'jsonrpc': '2.0',
                 'method': 'call',
@@ -59,8 +65,8 @@ class Tawssil:
                         'statut',
                         'return_reason',
                     ],
-                    'offset': offset,
-                    'limit': 80,
+                    'offset': OFFSET, # offset means "start from x+1 to limit, Ex: if offset=0 and limit=80 result will be from 1-80, offset=80 result: 81-160...etc"
+                    'limit': limit, # max limit = 80
                     'sort': '',
                     'context': {
                         'lang': 'fr_FR',
@@ -77,22 +83,32 @@ class Tawssil:
             response = requests.post('https://portail.tawssil.ma/web/dataset/search_read', cookies=self.session.cookies, json=json_data)
             
             try:
+                if not response.json()["result"]["length"]:
+                    # if length is 0 means the last page was the previous
+                    return packages
+                
                 packages["total"] += response.json()["result"]["length"]
                 packages["list"].extend(response.json()["result"]["records"])
-                return {
-                    "total": response.json()["result"]["length"],
-                    "packages": response.json()["result"]["records"]
-                }
+              
             except:
                 print(response.content)
+                return
         
-            offset += 80
+            OFFSET += limit # add limit X to offset to move to the next page
             
-username = "PARApharma"
-password = "20Parapharmacie@Leader24"
+        return packages
+            
+            
+    def savePackages(self, packages:list, filename="tawssil_packages.json"):
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(packages["list"], f, ensure_ascii=False, indent=4)
+        
 
-tawssil = Tawssil(username, password)
+tawssil = Tawssil("username", "password")
 
 packages = tawssil.getPackages()
 
-print(packages)
+print("Total Packages:", packages["total"])
+
+tawssil.savePackages(packages)
+
