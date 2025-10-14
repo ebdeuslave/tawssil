3fc5818ab3b5ae85aa80aead98e58f0e4596a64d
ebdeuslave <abderrahim.essaouaf@gmail.com>
2025-10-14 15:31:43 +0100

    backup shipments history

--- pushToGithub.py
+++ pushToGithub.py
@@ -7,4 +7,4 @@ def autoPush(fileToAdd, commitMsg):
     print(os.system(f'git add {fileToAdd} && git commit -m "{commitMsg}" && git push'))
 
 if __name__ == '__main__':
-    autoPush(".", "add package_id to order_data")
+    autoPush(".", "update")
